b2b2551825cd6a3751dd3e172f6178e7196c34f4
Emmanuel Caiola <emmanuel.caiola@free.fr>
2023-06-14 16:41:01 +0200

    filtres modif

--- js/factory/menuSearch.js
+++ js/factory/menuSearch.js
@@ -1,30 +1,53 @@
 const selectIngredient = document.querySelectorAll('#choix1');
 let filtres = [];
 
-function closeFilter() {
 
-}
-let increment = 0;
 
+let choix = '';
 selectIngredient.forEach(element => {
     element.addEventListener("click", function () {
-        increment++;
-        filtres.push(element.innerText);
-        const filtre = document.querySelector('.filtres');
-        const selectFiltre = document.createElement('div');
-        selectFiltre.setAttribute('id', `div${increment}`);
-        selectFiltre.innerHTML = `${element.innerText}<i id=${increment} class="fa-solid fa-xmark"></i>`;
-        filtre.appendChild(selectFiltre);
+        //recupération chaine seletionnée en supprimante les espace             
+        choix = element.innerText.replace(/\s/g, '');
 
-       document.getElementById(increment).addEventListener('click', function (e) {
-        console.log(e.target.id);
-            document.getElementById(`div${e.target.id}`).style.display = 'none';
-            console.log(e.target.id);
+        //recherche dans le tableau si la selection à déjà été selectionné
+        const found = filtres.indexOf(choix);
+      
+console.log(found);
+        if (found == -1) {
+            //insertion dans un tableau       
+            filtres.push(choix);
+            console.log(filtres);
 
-        })
+            //création des éléments du DOM avec id du nom de l'élément selectionné        
+            const filtre = document.querySelector('.filtres');
+            const selectFiltre = document.createElement('div');
+            selectFiltre.setAttribute('id', `div${choix}`);
+            selectFiltre.innerHTML = `${element.innerText}<i id=${choix} class="fa-solid fa-xmark"></i>`;
+            filtre.appendChild(selectFiltre);
 
+            //ajout d'un addEvenListner sur la croix de fermeture de l'élément
 
-        console.log(filtres);
-        console.log(element.innerText);
-    });
-})
+  
+             const fermetureFiltre=function (e) {
+                //fermeture de l'élément.
+                document.getElementById(choix).removeEventListener('click',fermetureFiltre);
+                
+                document.getElementById(`div${e.target.id}`).style.display = 'none';
+              
+                
+                //suppression dans le tableau de la selection
+                console.log(filtres);
+                console.log(e.target.id);
+                console.log(filtres.indexOf(e.target.id));
+                filtres.splice(filtres.indexOf(e.target.id), 1);
+                console.log(filtres);
+              
+                
+            }
+            
+            document.getElementById(choix).addEventListener('click',fermetureFiltre);
+            
+        }
+    })
+});
+
