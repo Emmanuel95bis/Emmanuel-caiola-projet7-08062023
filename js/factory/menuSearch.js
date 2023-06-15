25ab64861fc4acbdfa1b62abb5889082901b4f40
Emmanuel Caiola <emmanuel.caiola@free.fr>
2023-06-15 11:18:46 +0200

    modif bug filtres

--- js/factory/menuSearch.js
+++ js/factory/menuSearch.js
@@ -4,9 +4,27 @@ let filtres = [];
 
 
 let choix = '';
+
 selectIngredient.forEach(element => {
+    //recupération chaine selletionnée en supprimant les espaces             
+    choix = element.innerText.replace(/\s/g, '');
+  //création des éléments du DOM avec id du nom de l'élément selectionné        
+  const filtre = document.querySelector('.filtres');
+  const selectFiltre = document.createElement('div');
+   selectFiltre.setAttribute('id', `div${choix}`);
+  selectFiltre.innerHTML = `${element.innerText}<i id=${choix} class="fa-solid fa-xmark"></i>`;
+
+  filtre.appendChild(selectFiltre);
+  document.getElementById(`div${choix}`).style.display = 'none';
+
+
+});
+
+
+selectIngredient.forEach(element => {
+
     element.addEventListener("click", function () {
-        //recupération chaine seletionnée en supprimante les espace             
+        //recupération chaine seletionnée en supprimant les espaces             
         choix = element.innerText.replace(/\s/g, '');
 
         //recherche dans le tableau si la selection à déjà été selectionné
@@ -18,34 +36,27 @@ console.log(found);
             filtres.push(choix);
             console.log(filtres);
 
-            //création des éléments du DOM avec id du nom de l'élément selectionné        
-            const filtre = document.querySelector('.filtres');
-            const selectFiltre = document.createElement('div');
-            selectFiltre.setAttribute('id', `div${choix}`);
-            selectFiltre.innerHTML = `${element.innerText}<i id=${choix} class="fa-solid fa-xmark"></i>`;
-            filtre.appendChild(selectFiltre);
+            document.getElementById(`div${choix}`).style.display = 'flex';
 
             //ajout d'un addEvenListner sur la croix de fermeture de l'élément
-
-  
              const fermetureFiltre=function (e) {
                 //fermeture de l'élément.
-                document.getElementById(choix).removeEventListener('click',fermetureFiltre);
-                
                 document.getElementById(`div${e.target.id}`).style.display = 'none';
               
-                
                 //suppression dans le tableau de la selection
-                console.log(filtres);
-                console.log(e.target.id);
-                console.log(filtres.indexOf(e.target.id));
-                filtres.splice(filtres.indexOf(e.target.id), 1);
-                console.log(filtres);
+               console.log("ZZZZZZZZZZZZZZZZZZZ");
+                console.log("target : "+e.target.id);
+                console.log("tableau : "+filtres);
               
-                
+                filtres.splice(filtres.indexOf(e.target.id), 1);
+              
+                console.log("tableau : "+filtres);
+                console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+                document.getElementById(e.target.id).removeEventListener('click',fermetureFiltre);
             }
-            
             document.getElementById(choix).addEventListener('click',fermetureFiltre);
+            console.log('ajoutchoix :'+ choix);
+           
             
         }
     })
